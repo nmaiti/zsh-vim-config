20f298d959cb727d7b0d8d1c27957697cb6449b4
Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>
2022-06-12 17:59:57 +0530

    Add machine type based install
    Signed-off-by: Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -8,24 +8,40 @@
 # Args :
 #
 # Creation Date : 02-01-2021
-# Last Modified : 12-06-22 12:37:07S
+# Last Modified : 12-06-22 15:36:58S
 #
 # Created By : Nabendu
 # Email : 1206581+nmaiti@users.noreply.github.com
 ####################################################################
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN"
+esac
+echo ${machine}
 
-if [ `uname` == 'Darwin' ]; then
+if [ $machine == 'Mac' ]; then
     echo ' ******* Mac detected ******* '
     brew install ctags
-else
+elif [ $machine == 'MinGw' ]; then
+    hash cscope 2>/dev/null && hash ctags 2>/dev/null && clang-format fzf 2>/dev/null && hash bat 2>/dev/null && hash fzf 2>/dev/null && hash rg 2>/dev/null
+    if [[ $? -ne 0 ]];then
+        echo ' Binaries are missing (cscope, ctags, clang-format, bat, ripgrep, fzf)'
+        echo ' Install Manually from LINK and start this script)'
+        exit 0
+    fi
+elif [ $machine == 'Linux' ]; then
     which apt-get 2>/dev/null
     is_debian=$?
 
     #  Wsl version need update in dns resolution file
     if [[ -n "$IS_WSL" || -n "$WSL_DISTRO_NAME" ]]; then
         echo "This is WSL modifying /etc/wsl.conf & /etc/resolv.conf"
-	    echo '[network]\ngenerateResolvConf = false'| sudo tee -a /etc/wsl.conf
-	    echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+        echo '[network]\ngenerateResolvConf = false'| sudo tee -a /etc/wsl.conf
+        echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
     else
         echo "This is not WSL"
     fi
@@ -64,47 +80,62 @@ else
             sudo yum install -y ctags
         fi
     fi
-fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-if [[ -e ~/.zshrc ]]; then
-    echo 'Backup zshrc to ~/.zshrc.bak'
-    mv ~/.zshrc ~/.zshrc.bak
-fi
-
-# install zshrc
-if [[ ! -d ~/.zshrc ]];then
-    cp $(pwd)/zshrc_src ~/.zshrc
-    sed -i "s/XYZZ/$USER/g" ~/.zshrc
-    echo "exec zsh" >> ~/.bashrc
-#    sudo chsh -s $(which zsh)
-fi
-
-if [[ -e ~/.vimrc ]]; then
-    echo 'Backup vimrc to ~/.vimrc.bak'
-    mv ~/.vimrc ~/.vimrc.bak
-fi
-
-if [[ -d ~/.vim ]]; then
-    echo 'Backup vim to ~/.vim.bak'
-    mv ~/.vim ~/.vim.bak
-fi
-
-if [[ ! -d ~/.vimrc ]];then
-    cp $(pwd)/vimrc_src ~/.vimrc
-
-    mkdir ~/.vim
-    cp -rf $(pwd)/vim_src/headers ~/.vim/
-
-   #    ln -s $(pwd)/vimrc_src ~/.vimrc
-   #    ln -s $(pwd)/vim_src ~/.vim
-   echo '#########################'
-   echo 'install finished.'
-   echo 'If you want to install vim-go, Please install golang first,'
-   echo 'then running: go get -u github.com/jstemmer/gotags'
-   exit 0
 else
-    echo '~/vimrc does not exist'
-    exit 0
+
+    echo "This is not Known Machine type"
+fi
+
+
+if [ $machine != 'UNKNOWN' ]; then
+
+    if [[ -d ~/.oh-my-zsh ]]; then
+
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+        ZSH_CUSTOM='~/.oh-my-zsh/custom'
+        #    if [[ -e ~/.zshrc ]]; then
+        echo "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+        git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+        if [[ -e ~/.zshrc ]]; then
+            echo 'Backup zshrc to ~/.zshrc.bak'
+            mv ~/.zshrc ~/.zshrc.bak
+        fi
+
+        # install zshrc
+        if [[ ! -d ~/.zshrc ]];then
+            cp $(pwd)/zshrc_src ~/.zshrc
+            sed -i "s/XYZZ/$USER/g" ~/.zshrc
+            echo "exec zsh" >> ~/.bashrc
+            #    sudo chsh -s $(which zsh)
+        fi
+    fi
+
+    if [[ -e ~/.vimrc ]]; then
+        echo 'Backup vimrc to ~/.vimrc.bak'
+        mv ~/.vimrc ~/.vimrc.bak
+    fi
+
+    if [[ -d ~/.vim ]]; then
+        echo 'Backup vim to ~/.vim.bak'
+        mv ~/.vim ~/.vim.bak
+    fi
+
+    if [[ ! -d ~/.vimrc ]];then
+        cp $(pwd)/vimrc_src ~/.vimrc
+
+        mkdir ~/.vim
+        cp -rf $(pwd)/vim_src/headers ~/.vim/
+
+       #    ln -s $(pwd)/vimrc_src ~/.vimrc
+       #    ln -s $(pwd)/vim_src ~/.vim
+       echo '#########################'
+       echo 'install finished.'
+       echo 'If you want to install vim-go, Please install golang first,'
+       echo 'then running: go get -u github.com/jstemmer/gotags'
+       exit 0
+   else
+       echo '~/vimrc does still present'
+       exit 0
+    fi
 fi
